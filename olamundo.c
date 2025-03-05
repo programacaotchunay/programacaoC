aca47eb087c331c77bae8dea9ed69e20bb55bab8
programacaotchunay <josuesantossntp@gmail.com>
2025-03-05 22:59:45 +0000

    Operadores criar

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+    int main(){
 
+        int numero1 =1;
+        printf("Antes incremento: %d\n", numero1);
+
+        numero1++;
+        printf("Apos incremento: %d\n", numero1);
+
+        numero1--;
+        printf("Apos decremento: %d\n", numero1);
+
+
+    
+
+
+    }
